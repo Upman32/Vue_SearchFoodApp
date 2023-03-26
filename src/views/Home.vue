993ec1e69386f41ac0cbbbd26b379ebc3c1c7eae
Kirill Seredin <k.seredin@rambler.ru>
2023-03-26 12:28:15 +0530

    Created vue project, installed router,vuex. Did home page and started to use router and vuex

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-red-200 w-full"
+      placeholder="Search for meals"
+    />
+
+    <div class="mt-2 flex justify-center gap-1">
+      <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
+</template>
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+</script>
